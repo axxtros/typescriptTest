a8f3559a3911c24b52c33003be3ffc6c3060c438
ghofer <gabor.hofer@frontsoft.hu>
2016-12-12 08:45:35 +0100

    Egy osztályt tettem a kliens oldali ts-be, fordul, meghívódik, működik.

--- typescriptdemoproject/ballwar/public/javascripts/Test.js
+++ typescriptdemoproject/ballwar/public/javascripts/Test.js
@@ -1,6 +1,17 @@
+var TestClass = (function () {
+    function TestClass(name) {
+        this.oname = name;
+    }
+    TestClass.prototype.greet = function () {
+        return "Hello " + this.oname + "!";
+    };
+    return TestClass;
+}());
 function writeLogText(writeLogText) {
     console.log('My LOG: ' + writeLogText);
 }
 writeLogText('Hello...');
 writeLogText('Ez egy node kliens oldali kiírás.');
+var testObject = new TestClass("Gábor");
+console.log(testObject.greet());
 //# sourceMappingURL=Test.js.map
--- typescriptdemoproject/ballwar/public/javascripts/Test.ts
+++ typescriptdemoproject/ballwar/public/javascripts/Test.ts
@@ -1,7 +1,22 @@
-﻿function writeLogText(writeLogText) {
+﻿class TestClass {
+    oname: string;
+
+    constructor(name: string) {
+        this.oname = name;
+    }
+
+    greet() {
+        return "Hello " + this.oname + "!";
+    }
+}
+
+function writeLogText(writeLogText) {
     console.log('My LOG: ' + writeLogText);
 }
 
 writeLogText('Hello...');
 
 writeLogText('Ez egy node kliens oldali kiírás.');
+
+var testObject = new TestClass("Gábor");
+console.log(testObject.greet());
